f1da67f83218d612ba640668c677f3c394d4da27
Suraj-729 <surajkumarsahoo729@gmail.com>
2024-12-08 14:17:20 +0530

    git readme and Authendication Api complete

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -27,9 +27,27 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
+        select: false,
     },
 
     socketId: {
         type: String,
     },
 })
+
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
+    return token;
+}
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+}
+
+const userModel = mongoose.model('user' , userSchema);
+
+module.exports = userModel;
