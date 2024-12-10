bfd5c1183cb327f160b47cc67f1ed7095a610afe
Suraj-729 <surajkumarsahoo729@gmail.com>
2024-12-10 21:19:24 +0530

    Authendication Complete for User Model

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -30,15 +30,28 @@
 
 const userModel = require('../models/user.model');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 module.exports.authUser = async (req, res, next) => {
-    try {
-        // Extract token from cookies or Authorization header
-        const token = req.cookies.token || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
+
+
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
         if (!token) {
             return res.status(401).json({ message: 'Unauthorized: Token missing' });
         }
 
+
+
+       const isBlacklisted = await userModel.findOne({ token: token });
+        if(isBlacklisted){
+            return res.status(401).json({ message: 'Unauthorized: Access' });
+        }
+
+
+    try {
+        // Extract token from cookies or Authorization header
+        
+         
         // Verify token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         console.log('Decoded Token:', decoded);
@@ -51,11 +64,11 @@ module.exports.authUser = async (req, res, next) => {
 
         // Attach user to request
         req.user = user;
-        console.log('Authenticated User:', user);
+       // console.log('Authenticated User:', user);
 
         next(); // Proceed to next middleware or route handler
     } catch (err) {
-        console.error('JWT Verification Error:', err.message);
+        //console.error('JWT Verification Error:', err.message);
         res.status(401).json({ message: 'Unauthorized Access' });
     }
 };
