8aa637f401699a8cb285a1f4ab07de90b55f5125
Suraj-729 <surajkumarsahoo729@gmail.com>
2024-12-15 10:53:27 +0530

    git captain Changes

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -3,6 +3,8 @@
 const userModel = require('../models/user.model');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const blackListTokenModel = require('../models/blacklistToken.model')
+const captainModel = require('../models/captain.model')
 
 module.exports.authUser = async (req, res, next) => {
 
@@ -14,7 +16,7 @@ module.exports.authUser = async (req, res, next) => {
 
 
 
-       const isBlacklisted = await userModel.findOne({ token: token });
+       const isBlacklisted = await blackListTokenModel.findOne({ token: token });
         if(isBlacklisted){
             return res.status(401).json({ message: 'Unauthorized: Access' });
         }
@@ -44,3 +46,46 @@ module.exports.authUser = async (req, res, next) => {
         res.status(401).json({ message: 'Unauthorized Access' });
     }
 };
+
+module.exports.authCaptain = async (req, res, next) => {
+
+
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'Unauthorized: Token missing' });
+        }
+
+
+
+       const isBlacklisted = await blackListTokenModel.findOne({ token: token });
+        if(isBlacklisted){
+            return res.status(401).json({ message: 'Unauthorized: Access' });
+        }
+
+
+    try {
+        // Extract token from cookies or Authorization header
+        
+         
+        // Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        //console.log('Decoded Token:', decoded);
+
+        // Find user in the database
+        const captain = await captainModel.findById(decoded._id);
+        if (!captain) {
+            return res.status(404).json({ message: 'Captain not found' });
+        }
+
+        // Attach user to request
+        req.captain = captain;
+       // console.log('Authenticated User:', user);
+
+        return next(); // Proceed to next middleware or route handler
+    } catch (err) {
+        //console.error('JWT Verification Error:', err.message);
+        res.status(401).json({ message: 'Unauthorized Access' });
+    }
+
+
+}
